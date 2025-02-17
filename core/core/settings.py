089f205cec04b04a7549f203217ad213bb216cf4
Ester Araújo <esteraraujo0805@gmail.com>
2025-02-16 23:10:01 -0300

    first commit

---
+++ core/core/settings.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+# Defina BASE_DIR como o diretório raiz do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configurações básicas
+SECRET_KEY = 'sua_chave_secreta_aqui'
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Configurações de aplicativos
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'pacientes',  # App pacientes registrado aqui
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Configuração de URLs
+ROOT_URLCONF = 'core.urls'
+
+# Configurações de templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Configuração do WSGI
+WSGI_APPLICATION = 'core.wsgi.application'
+
+# Banco de dados
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Arquivos estáticos e de mídia
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Mensagens
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.SUCCESS: 'bg-green-50 text-green-700',
+    constants.ERROR: 'bg-red-50 text-red-700',
+}
